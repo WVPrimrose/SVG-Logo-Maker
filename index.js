269f21c7192ba19e31876b396094e826e0adc67f
Katie Buchholz <kzbindy@hotmail.com>
2024-07-03 20:11:00 -0400

    got the index.js correctly syntaxed but still needed to work on it; got the logo.js running with the class constructor going; still needs to do some work, but think i got it

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const inquirer = require ('inquirer')
+const inquirer = require('inquirer')
+const Shape = require('./lib/logo')
 
 inquirer
-    .prompt([])
-    {
-        name: 'text'
-        type: 'input'
-        message: 'Enter text up to three characters'
-    }
-    {
-        name: 'text-color'
-        type: 'list'
-        message: 'Select one of the following text colors:'
-        choices: 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'white', 'gray', 'brown'
-    }
-    {
-        name: 'shape'
-        type: 'list'
-        message: 'Select one of the following shapes:'
-        choices: 'triangle', 'circle', 'square'
-    }
-    {
-        name: 'text-color'
-        type: 'list'
-        message: 'Select one of the following text colors:'
-        choices: 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'white', 'gray', 'brown'
-    }
+    .prompt([
+        {
+            name: 'text',
+            type: 'input',
+            message: 'Enter text up to three characters'
+        },
+        {
+            name: 'textColor',
+            type: 'list',
+            message: 'Select one of the following text colors:',
+            choices: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'white', 'gray', 'brown']
+        },
+        {
+            name: 'shape',
+            type: 'list',
+            message: 'Select one of the following shapes:',
+            choices: ['triangle', 'circle', 'square']
+        },
+        {
+            name: 'shapeColor',
+            type: 'list',
+            message: 'Select one of the following shape colors:',
+            choices: ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'white', 'gray', 'brown']
+        },
+
+    ])
+    .then(({text, textColor, shape, shapeColor})=>{
+
+    })

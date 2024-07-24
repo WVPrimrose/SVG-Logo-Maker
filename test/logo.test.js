b6ea5817c191bf5ccfa5ce8981014ff0fc30ab2a
Katie Buchholz <kzbindy@hotmail.com>
2024-07-24 17:55:17 -0400

    finally got the test to pass

--- test/logo.test.js
+++ test/logo.test.js
@@ -1,14 +1,12 @@
-const { Square, Triangle, Circle } = require('../lib/logo.js')
-// new instance for testing
-const triangle1 = new Triangle('ABC', 'black', 'triangle', 'green');
+const { Triangle } = require('../lib/logo.js')
 
 describe('testing shape class', () => {
     test('should render a green triangle', () => {
         const expectSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 <polygon points="150, 18 244, 182 56, 182" fill="green" />
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="black">'ABC'</text>
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="black">ABC</text>
 </svg>`;
-        const triangle = new Triangle();
+        const triangle = new Triangle('ABC','black', 'triangle', 'green');
         triangle.setColor('green');
         const actualSvg = triangle.render();
         expect(actualSvg).toEqual(expectSVG);

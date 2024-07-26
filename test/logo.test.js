dfcf57c1ad7c9b3fc0e0d68e7286acedb74513e3
Katie Buchholz <kzbindy@hotmail.com>
2024-07-25 22:04:06 -0400

    fixed small change to the test

--- test/logo.test.js
+++ test/logo.test.js
@@ -4,7 +4,7 @@ describe('testing shape class', () => {
     test('should render a green triangle', () => {
         const expectSVG = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 <polygon points="150, 18 244, 182 56, 182" fill="green" />
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="black">ABC</text>
+<text x="150" y="175" font-size="60" text-anchor="middle" fill="black">ABC</text>
 </svg>`;
         const triangle = new Triangle('ABC','black', 'triangle', 'green');
         triangle.setColor('green');
